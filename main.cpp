401264e539b3d296c01b8339584035384cd52648
Barsbek Azamatov <bars.moderside@gmail.com>
2026-03-01 17:21:47 -0800

    Add Movie class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 // COMSC-210 | Lab 15 | Barsbek
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Movie {
+private:
+    string m_screenWriter{};
+    string m_title{};
+    int m_releasedYear{};
+public:
+    // Setter
+    void setScreenWriter(const string& screenWriter) { m_screenWriter = screenWriter; }
+    void setTitle(const string& title) { m_title = title; }
+    void setReleasedYear(const int releasedYear) { m_releasedYear = releasedYear; }
+    //
+};
 
 int main() {
     return 0;
