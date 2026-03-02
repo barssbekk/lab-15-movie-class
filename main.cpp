e951e34da37b0d22ee87c8684acebe24bbe075b5
Barsbek Azamatov <bars.moderside@gmail.com>
2026-03-01 17:25:48 -0800

    Add getter/setter

--- main.cpp
+++ main.cpp
@@ -16,7 +16,10 @@ public:
     void setScreenWriter(const string& screenWriter) { m_screenWriter = screenWriter; }
     void setTitle(const string& title) { m_title = title; }
     void setReleasedYear(const int releasedYear) { m_releasedYear = releasedYear; }
-    //
+    // Getter
+    string getScreenWriter() const { return m_screenWriter; }
+    string getTitle() const { return m_title; }
+    int getReleasedYear() const {return m_releasedYear; }
 };
 
 int main() {
